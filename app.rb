8d43163c14359f3d07773eae866217be4e7c341f
Kevin <kevhines@gmail.com>
2021-02-23 16:57:21 -0500

    commit tree 1bc59e5526e74dd90f7f26aea94a3ebe0f6db795

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    configure do
+        enable :sessions
+        set :session_secret, "apple cart"
+      end 
+
+    get '/' do
+        erb :index
+    end
+
+    post '/checkout' do
+        @item = params["item"]
+        session["item"] = @item
+        @item
+    end
 end
